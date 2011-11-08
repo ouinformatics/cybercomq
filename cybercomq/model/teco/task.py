e572f19d0717954707ac3bf72e9bcfee7572a448
Mark Stacy <markstacy@ou.edu>
2011-11-08 12:53:21 -0600

    teco run

---
+++ cybercomq/model/teco/task.py
@@ -0,0 +1,47 @@
+from celery.task import task
+#import time 
+#from StringIO import StringIO
+#from cybercom.api.catalog import datalayerutils as dl
+from urllib2 import urlopen
+from cybercom.data.catalog import datalayer
+from subprocess import call
+#call(["ls", "-l"])
+import os
+ip-129-15-40-58.rccc.ou.edu
+if os.uname()[1] == 'ip-129-15-40-58.rccc.ou.edu':
+    basedir = '/Users/mstacy/Desktop/TECO_HarvardForest/'
+elif os.uname()[1] == 'earth.rccc.ou.edu':
+    basedir = '/scratch/cybercom/model/teco/'
+#earth_Teco = '/scratch/cybercom/model/teco/runTeco'
+#mstacyTeco = '/Users/mstacy/Desktop/TECO_HarvardForest/'#runTeco'
+
+@task(serializer="json")
+def add(x, y):
+    return x + y
+    #md= datalayer.Metadata()
+    #return md.Search('dt_location')
+    #return x + y 
+
+@task
+def runTeco(param):
+    ''' run teco model 
+        param = {url to files files required to run model}
+    '''
+    try:
+        os.chdir(basedir)
+        return call([basedir + 'runTeco']) 
+    except:
+        raise
+
+@task
+def getLocation(commons_id=None):
+    md=datalayer.Metadata()
+    if commons_id != None:
+        whr = 'commons_id = %d' % (commons_id)
+        return md.Search('dt_location',where = whr)
+    return md.Search('dt_location')
+
+@task
+def sleep(s):
+    time.sleep(s)
+    return None
