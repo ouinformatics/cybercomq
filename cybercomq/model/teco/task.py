2ef9a7f2c60dc85387463e3f2e927f2d7103d93f
Mark Stacy <markstacy@ou.edu>
2011-11-10 11:38:52 -0600

    task update

--- cybercomq/model/teco/task.py
+++ cybercomq/model/teco/task.py
@@ -12,31 +12,25 @@ if os.uname()[1] == 'ip-129-15-40-58.rccc.ou.edu':
     basedir = '/Users/mstacy/Desktop/TECO_HarvardForest/'
 elif os.uname()[1] == 'earth.rccc.ou.edu':
     basedir = '/scratch/cybercom/model/teco/'
-#earth_Teco = '/scratch/cybercom/model/teco/runTeco'
-#mstacyTeco = '/Users/mstacy/Desktop/TECO_HarvardForest/'#runTeco'
 
 @task(serializer="json")
 def add(x, y):
     return x + y
-    #md= datalayer.Metadata()
-    #return md.Search('dt_location')
-    #return x + y 
-
 
 @task(serilizer="json")
 def getTecoinput():
     '''Currently setup up for demo specific input files'''
     try:
-        #os.chdir(basedir)
         md=datalayer.Metadata()
         sWhere = "var_id = 'URL' and event_id in (select event_id from dt_event where cat_id = %d or cat_id = %d) " % (1446799,1446801) 
         res = md.Search('dt_result',where=sWhere,column=['var_id','result_text'])
         for url in res:
             temp= url['result_text'].split("/")
             fname = temp[len(temp)-1]
-            cmd = "wget -r -O " + basedir + fname + " " + url['result_text']
-            print cmd
-            a= commands.getoutput(cmd) #call(cmd)
+            filepath = basedir + fname
+            a = urlopen(url['result_text'])
+            f1= open(filepath,'w')
+            f1.write(a.read())
         return True 
     except:
         raise
@@ -47,7 +41,8 @@ def runTeco(param):
     '''
     try:
         os.chdir(basedir)
-        return call([basedir + 'runTeco']) 
+        call([basedir + 'runTeco'])
+        return 'TECO Model run Complete'
     except:
         raise
 
