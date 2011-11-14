d23f69240450232ebb07eeb88b95a17118bd2d0b
Jonah Duckles <jduckles@ou.edu>
2011-11-14 12:23:59 -0600

    Allow string dates

--- cybercomq/static/tasks.py
+++ cybercomq/static/tasks.py
@@ -7,7 +7,7 @@ import os
 
 BROKER_URL = "amqplib://jduckles:cybercommons@fire.rccc.ou.edu/cybercom_test"
 
-@task
+@task(serializer='json')
 def modiscountry(product, country, start_date, end_date, outpath=None):
     if socket.gethostname() == 'static.cybercommons.org' and not outpath:
         outpath = '/static/request'
@@ -15,6 +15,15 @@ def modiscountry(product, country, start_date, end_date, outpath=None):
         outpath = os.getcwd()
     cat_ids = fz.catname2catid(product,'807')
     files = []
+    # Check to make sure we have date-time
+    if isinstance(start_date,datetime) or isinstance(end_date,datetime):
+        pass
+    else:
+        try:
+            start_date = datetime.strptime(start_date, '%Y-%m-%d')
+            end_date = datetime.strptime(end_date, '%Y-%m-%d')
+        except ValueError:
+            return 'Valid dates are in YYYY-mm-dd format'
     for cat_id in cat_ids:
         files += fz.getEventResult_Country(cat_id=cat_id, 
                         country=country, start_date=start_date, 
