00fc564808e0f31ea14534c25f576f35788c38cc
Mark Stacy <markstacy@ou.edu>
2011-12-01 15:40:38 -0600

    fix syntax error

--- cybercomq/model/teco/task.py
+++ cybercomq/model/teco/task.py
@@ -33,7 +33,7 @@ def initTECOrun(**kwargs):
         call(["mkdir",newDir])
         os.chdir(newDir)
         call(["ln","-s",basedir + "runTeco",newDir + "/runTeco"])
-        param = json.loads(urlopen("http://test.cybercommons.org/mongo/db_find/teco/siteparam/{'spec':{'site':'" + site + + "'}}/?callback=?").read())[0]
+        param = json.loads(urlopen("http://test.cybercommons.org/mongo/db_find/teco/siteparam/{'spec':{'site':'" + site + "'}}/?callback=?").read())[0]
         set_site_param(initTECOrun.request.id,param)
         #call(["ln","-s",basedir + "sitepara_tcs.txt",newDir + "/sitepara_tcs.txt"])
         call(["ln","-s",basedir + "initial_opt.txt",newDir + "/initial_opt.txt"])
