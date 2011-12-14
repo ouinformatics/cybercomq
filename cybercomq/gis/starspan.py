948ce0383f03facfb583729ec1872fd5841f10f7
Jonah Duckles <jduckles@ou.edu>
2011-12-14 16:27:41 -0600

    starspan running task for gis queue

---
+++ cybercomq/gis/starspan.py
@@ -0,0 +1,43 @@
+from celery import task
+from subprocess import call
+import shlex 
+from pymongo import Connection
+import os
+from StringIO import StringIO
+
+def starspancmd( params, doshlex=True ):
+    """
+    Setup and run a starspan polygon-raster overlay command.
+    params = {
+    'dbname': 'cybercom',  
+    'dbhost': 'fire.rccc.ou.edu',
+    'table': 'public.floras',
+    'raster': "/ddn_1/data/worldclim/30s/bio_1.tif",
+    'outfile': 'tmp.csv',
+    'stats': "avg stdev min max sum median",
+    'field': "ref_no",
+    'geom': '201',
+    }
+    """
+    geom = params['geom']
+    if geom: # If geom supplied as string or list, extract all geometries matching
+        if type(geom) is list:
+            params['geom'] = str(geom).replace('[','(').replace(']',')')
+            params['query'] = "select wkb_geometry, %(field)s from %(table)s where %(field)s in %(geom)s" % (params)
+        elif type(geom) is str:
+             params['query'] = "select wkb_geometry, %(field)s from %(table)s where %(field)s = %(geom)s" % (params)
+    else: # Otherwise select all records and process accordingly
+        params['query'] = "select wkb_geometry, %(field)s from %(table)s" % (params)
+
+
+    command = 'starspan2 --verbose --vector "PG:dbname=%(dbname)s host=%(dbhost)s tables=%(table)s" --sql "%(query)s" --raster %(raster)s --stats %(outfile)s %(stats)s --fields %(field)s' % (params)
+    if doshlex:
+        return shlex.split(command)
+    else:
+        return command
+
+@task
+def execute_starspan(params, loglocation='mongo'):
+    logname = os.tmpnam()
+    logfile = open(logname, 'w')
+    call(starspancmd(params),stdout=logfile,stderr=subprocess.STDOUT)
